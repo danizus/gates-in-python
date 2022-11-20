09f613b2beb8f70f48738621f2f905468198e1a8
daniyal ahmed <111660699+danizus@users.noreply.github.com>
2022-11-20 02:57:40 -0800

    Add files via upload

---
+++ program.py
@@ -0,0 +1,27 @@
+
+def _or(x,y):
+    c=x or y
+    return c
+def _and(x,y):
+    c=x and y
+    return c
+def _nor(x,y):
+    c=not(_or(x,y))
+    return c
+def _nand(x,y):
+    c=not(_and(x,y))
+    return c
+def _xor(x,y):
+    c=_and(_or(x,y),_nand(x,y))
+    return c
+def _xnor(x,y):
+    c=not(_xor(x,y))
+    return c
+#x and y are the inputs 
+x=1 
+y=0
+#example
+print(_or(_nand(x,y),_xor(x,y)))
+
+
+    
